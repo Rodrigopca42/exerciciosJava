20f6733a4569472f51d08607303e445e44958082
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-06-18 09:20:57 -0300

    readme
    readme

--- fundamentos/src/temperaruta/Conversao.java
+++ fundamentos/src/temperaruta/Conversao.java
@@ -1,9 +1,16 @@
 package temperaruta;
 
+import java.util.Scanner;
+
 public class Conversao {
+    
+
     public static void main(String[] args) {
 
-        double Fahrenheit = 75.00;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em Fahrenheit:");
+        double Fahrenheit = teclado.nextDouble();
         double Celsius = 28.15;
         final double x = 5.0;
         final double y = 9.0;
@@ -12,8 +19,9 @@ public class Conversao {
 
         double conv = (Fahrenheit - z) * x / y;
 
-        System.out.println("Temperatura em Celsius = °" + conv );
+        System.out.printf("Temperatura em Celsius = "+ conv +"°C%n");
 
+        teclado.close();
         double conv1 = y * Celsius / x + z;
         System.out.println("Temperatura em Fahrenheit = °" + conv1);
 
--- fundamentos/src/temperaruta/Inferencia.java
+++ fundamentos/src/temperaruta/Inferencia.java
@@ -1,6 +1,7 @@
 package temperaruta;
 
 import java.util.Date;
+import java.util.Scanner;
 
 
 
@@ -15,13 +16,33 @@ public class Inferencia {
         String s = "Bom dia";
           System.out.println(s.concat("!!!"));
 
-        var nome = "Rodrigo";
+       /*  var nome = "Rodrigo";
         var sobrenome = "Cardoso";
         var idade = 43;
-        var salario = 3521.53;
+        var salario = 3521.53;  
 
-        System.out.printf("O senhor %s %s tem %d anos e ganha R$%.2f" ,nome, sobrenome, idade, salario);
+        System.out.printf("O senhor %s %s tem %d anos e ganha R$%.2f" ,nome, sobrenome, idade, salario); */
 
+        Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Digite seu nome :");
+        teclado.nextLine();
+        String nome = teclado.nextLine();
+      
+
+        System.out.println("Digite seu sobrenome :");
+        teclado.nextLine();
+        String sobrenome = teclado.nextLine();
+        
+
+        System.out.println("Digite sua idade :");
+        int idade = teclado.nextInt();
+
+        System.out.println("Digite seu salário : R$");
+        float sal = teclado.nextFloat();
+
+        System.out.printf("O funcionario %s %s tem %d de idade  e ganha R$ %.2f de salário por mês", nome, sobrenome, idade, sal );
+
+        teclado.close();
     }
 }
